f9b977447c249e0eb507d4dfb4a83dfd75bf2be8
Vito Genovese <genovese.work@protonmail.com>
2023-11-14 10:34:43 +0100

    removing

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
 def t_home():
     return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
